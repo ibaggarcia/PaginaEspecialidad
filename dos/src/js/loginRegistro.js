f5ea837a3c82ec2783de19578414dbe399cd1ac8
JosueContreras16 <josuecgonzalez16@gmail.com>
2023-02-12 17:43:03 -0600

    .

---
+++ dos/src/js/loginRegistro.js
@@ -0,0 +1,6 @@
+const btnAbrirModal = document.querySelector("#abrir-modal");
+const modal = document.querySelector("#modal");
+
+btnAbrirModal.addEventListener("click",()=>{modal.showModal();
+})
+
