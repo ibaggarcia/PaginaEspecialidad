4d8811597cb675c795f01a6bfb46b91d6180e7d7
JosueContreras16 <josuecgonzalez16@gmail.com>
2023-02-13 18:27:54 -0600

    .

--- dos/src/js/loginRegistro.js
+++ dos/src/js/loginRegistro.js
@@ -1,16 +1,18 @@
-var modal = document.getElementById("modallogin");
-var btn = document.getElementById("abrir-modal");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
+document.querySelector('#cerrar-modal').addEventListener('click', function() {
+  document.querySelector('#modallogin').style.display = 'none';
+});
+
+document.querySelector('a[data-target="modallogin"]').addEventListener('click', function() {
+  document.querySelector('#modallogin').style.display = 'block';
+});
+
+$('a[data-target="modallogin"]').on('click', function() {
+  $('#modallogin').show();
+});
+
+$('#close-modal').on('click', function() {
+  $('#mi-modal').hide();
+});
