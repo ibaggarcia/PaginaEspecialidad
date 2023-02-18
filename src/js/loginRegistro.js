765513cc9b1a805218fb10bdfd2a299d147a72e6
JosueContreras16 <josuecgonzalez16@gmail.com>
2023-02-18 12:46:53 -0600

    jij

--- src/js/loginRegistro.js
+++ src/js/loginRegistro.js
@@ -1,15 +1,16 @@
+
 document.querySelector('#cerrar-modal').addEventListener('click', function() {
-  document.querySelector('#modallogin').style.display = 'none';
+  document.querySelector('#modalregistro').style.display = 'none';
 });
 
-document.querySelector('a[data-target="modallogin"]').addEventListener('click', function() {
-  document.querySelector('#modallogin').style.display = 'block';
+document.querySelector('a[data-target="modalregistro"]').addEventListener('click', function() {
+  document.querySelector('#modalregistro').style.display = 'block';
 });
 
-$('a[data-target="modallogin"]').on('click', function() {
-  $('#modallogin').show();
+$('a[data-target="modalregistro"]').on('click', function() {
+  $('#modalregistro').show();
 });
 
 $('#close-modal').on('click', function() {
-  $('#mi-modal').hide();
+  $('#modalregistro').hide();
 });
