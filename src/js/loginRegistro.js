b9703dd0471bb3179af8903db27e933acbaf6c8a
JosueContreras16 <josuecgonzalez16@gmail.com>
2023-02-19 10:09:28 -0600

    jij

--- src/js/loginRegistro.js
+++ src/js/loginRegistro.js
@@ -1,4 +1,4 @@
-document.querySelector('#cerrar-modal').addEventListener('click', function() {
+/*document.querySelector('#cerrar-modal').addEventListener('click', function() {
   document.querySelector('#modalregistro').style.display = 'none';
 });
 
@@ -13,3 +13,30 @@ $('a[data-target="modalregistro"]').on('click', function() {
 $('#close-modal').on('click', function() {
   $('#mi-modal').hide();
 });
+*/
+
+// Obtener el enlace que abre el modal
+var link = document.querySelector('[data-target="modallogin"]');
+
+// Obtener el modal que se abrirá
+var modal = document.getElementById(link.getAttribute('data-target'));
+
+// Obtener el botón de cerrar
+var close = modal.querySelector('.close');
+
+// Abrir el modal cuando se hace clic en el enlace
+link.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Cerrar el modal cuando se hace clic en el botón de cerrar
+close.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Cerrar el modal cuando se hace clic fuera de él
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
