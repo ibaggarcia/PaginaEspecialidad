340d8f2e6c0403924415ff76ebd56d877d8f6490
JosueContreras16 <josuecgonzalez16@gmail.com>
2023-02-19 17:57:16 -0600

    jij

--- src/js/loginRegistro.js
+++ src/js/loginRegistro.js
@@ -40,3 +40,38 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// Obtener el enlace que abre el modal
+var link = document.querySelector('[data-target="modalusuario"]');
+
+// Obtener el modal que se abrirá
+var modal = document.getElementById(link.getAttribute('data-target'));
+
+// Obtener el botón de cerrar
+var close = modal.querySelector('.close');
+
+// Abrir el modal cuando se hace clic en el enlace
+link.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Cerrar el modal cuando se hace clic en el botón de cerrar
+close.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Cerrar el modal cuando se hace clic fuera de él
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+  $(document).ready(function() {
+    $('a.desplegar-menu').click(function() {
+      $('#menu').toggleClass('oculto');
+      return false;
+    });
+  });
+
